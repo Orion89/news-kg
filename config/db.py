67f746b0a3404d41c189ff3bf2267a0706ed34e5
Leo Molina <leonardo.molina.v@gmail.com>
2024-04-09 18:00:26 -0400

    updated db variables

--- config/db.py
+++ config/db.py
@@ -6,4 +6,13 @@ from config.settings import settings
 # pgpassword = os.getenv('PGPASSWORD')
 # pgport = os.getenv('PGPORT')
 
-conn = psycopg.connect(conninfo=settings.POSTGRES_URL)
+if os.getenv('RAILWAY_ENVIRONMENT_NAME') == 'dev':
+    conn = psycopg.connect(
+        host=os.getenv('DEV_DBHOST'),
+        dbname=os.getenv('DEV_DBNAME'),
+        user=os.getenv('DEV_DBUSER'),
+        password=os.getenv('DEV_DBPASSWORD')
+    )
+elif os.getenv('RAILWAY_ENVIRONMENT_NAME') == 'production':
+    from config.settings import settings
+    conn = psycopg.connect(conninfo=settings.POSTGRES_URL)
