0e689dc0d69a3dea5e31a0c2e443b01a150119c4
Leo Molina <leonardo.molina.v@gmail.com>
2024-06-07 23:08:04 -0400

    mod db config

--- config/settings.py
+++ config/settings.py
@@ -8,19 +8,22 @@ class Settings(BaseSettings):
     # HASHING_ALGORITHM: str = os.environ.get("HASHING_ALGORITHM")
 
     # DB Settings
-    POSTGRES_USER: str = os.environ.get("PGUSER") # 
-    POSTGRES_PASSWORD: str = os.environ.get("PGPASSWORD") # 
-    POSTGRES_SERVER: str = os.environ.get("PGHOST") # 
-    POSTGRES_PORT: int = os.environ.get("PGPORT") # 
-    POSTGRES_DB: str = os.environ.get("PGDATABASE") # 
+    POSTGRES_USER: str = os.environ.get("PGUSER")  #
+    POSTGRES_PASSWORD: str = os.environ.get("PGPASSWORD")  #
+    POSTGRES_SERVER: str = os.environ.get("PGHOST")  #
+    POSTGRES_PORT: int = os.environ.get("PGPORT")  #
+    POSTGRES_DB: str = os.environ.get("PGDATABASE")  #
+    POSTGRES_PRIVATE_URL: str = os.environ.get("DATABASE_PRIVATE_URL")
 
     class Config:
         env_file = ".env"
 
     @property
     def POSTGRES_URL(self):
-        url = f"postgresql://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_SERVER}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+        # url = f"postgresql://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_SERVER}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+        url = f"{self.POSTGRES_PRIVATE_URL}"
         return url
 
-if os.getenv('RAILWAY_ENVIRONMENT_NAME') == 'production':
+
+if os.getenv("RAILWAY_ENVIRONMENT_NAME") == "production":
     settings = Settings()
